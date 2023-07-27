f5b8b6af8aea6a6426a209efbb2ead3c72fd5f1f
rlawodnr1109 <wodnr1109@naver.com>
2023-07-27 09:07:11 +0900

    refactor: signInPage 변수명 변경

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import NotFound from '../pages/NotFound'
-import SignIn from '../pages/SignIn'
+import SignIn from '../pages/SignInPage'
 
 function Router() {
   return (
